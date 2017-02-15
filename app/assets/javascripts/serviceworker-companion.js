baf8474d9420060b166e51318f0326ea730e7446
Masaki Takehara <morisot64@gmail.com>
2017-02-15 12:38:57 +0900

    web push

--- app/assets/javascripts/serviceworker-companion.js
+++ app/assets/javascripts/serviceworker-companion.js
@@ -4,3 +4,23 @@ if (navigator.serviceWorker) {
       console.log('[Companion]', 'Service worker registered!');
     });
 }
+
+navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+  serviceWorkerRegistration.pushManager
+  .subscribe({
+    userVisibleOnly: true,
+    applicationServerKey: window.vapidPublicKey
+  });
+});
+
+$(function(){
+  $(".webpush-button").on("click", function(e) {
+    navigator.serviceWorker.ready
+    .then((serviceWorkerRegistration) => {
+      serviceWorkerRegistration.pushManager.getSubscription()
+      .then((subscription) => {
+        $.post("/push", { subscription: subscription.toJSON(), message: "You clicked a button!" });
+      });
+    });
+  });
+});
